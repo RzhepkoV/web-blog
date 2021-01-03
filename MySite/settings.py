e8f5b790e2f6e8bfd0ec33d5278b5cad1c7fb902
Vladimir Rzhepko <23-rva@mail.ru>
2021-01-03 17:26:32 +0800

    Modified settings.py

--- MySite/settings.py
+++ MySite/settings.py
@@ -130,9 +130,9 @@ EMAIL_HOST_PASSWORD = 'vwkjosqnjtzlydzr'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 
 
 LOGIN_REDIRECT_URL = 'profile'
